074e77f34d8383a40ae405ef726af7b0d84db754
Johannes Pfister <johannes.pfister@josttech.ch>
2023-07-25 18:45:43 +0000

    Implemented different band modes and limits

--- brainfuck.py
+++ brainfuck.py
@@ -15,7 +15,7 @@ Brainfuck commands:
 - Decrement current cell
 > Move to next cell
 < Move to previous cell (here: stay at cell 0 if we are already at cell 0)
-[ if cell value is 0, move to next command after the corresponding ]. If value is not 0, continue with command after [ 
+[ if cell value is 0, move to next command after the corresponding ]. If value is not 0, continue with command after [
 ] jump to corresponding [
 , read value
 . output value
@@ -103,43 +103,123 @@ class Source(object):
     self.position=0
     return self
 
-def execute(code,data,outputMode,celllimit=0):
+class Band(object):
+  """
+  Object to store the data of a brainfuck program
+  """
+  def __init__(self,mode="infinit",bandlimit=0,initValue=0,celllimit=0):
+    """
+    mode should either be infinit, wrap, wait or error
+    in infinit mode, new cells get added each time the pointer moves pass a border
+    in wrap mode, the position is calculated modulo bandlimit
+    in wait mode, the position stays at the border and doesn't move pass it
+    in error mode, passing beyond the border causes a error
+
+    the bandlimit set a maximum number of cells. Ignored in infinit mode
+    The bandlimit 0 means in this modes:
+    - inifinit: Ignore
+    - wrap: going below 0 means wrap to the highest current used. Grow when go beyond the positive border
+    - wait: going below 0 means wait at 0. Grow when go beyond the positive border
+    - error: going below 0 means error. Grow when go beyond the positive border
+
+    The celllimit gives a maximum value per cell
+    """
+    if mode not in ["infinit", "wrap", "wait", "error"]:
+      raise Exception("Invalid bandmode "+mode)
+    self.initValue= initValue%celllimit if celllimit else initValue
+
+    self.mode=mode
+    self.cells=[self.initValue]
+    if mode=="infinit":
+      self.ncells=[self.initValue]
+    self.position=0
+    self.bandlimit=bandlimit
+
+    self.celllimit=celllimit
+
+  def movePositive(self):
+    self.position += 1
+    if self.position==len(self.cells):
+      if self.bandlimit==0 or self.mode=="inifinit":
+        self.cells.append( self.initValue )
+      elif self.bandlimit==self.position:
+        if self.mode=="wrap": self.position = 0
+        if self.mode=="wait": self.position = self.position-1
+        if self.mode=="error": raise Exception("Reached out of bounds location")
+  def moveNegative(self):
+    self.position -= 1
+    if self.mode=="inifinit":
+      if self.position==-len(self.ncells)-1:
+        self.ncells.append( self.initValue )
+    elif self.position == -1:
+      if self.mode=="wrap":
+        if self.bandlimit==0:
+          self.position = len(self.cells)-1
+        else:
+          if len(self.cells) < self.bandlimit:
+            self.cells = self.cells + [ self.initValue for i in range(self.bandlimit-len(self.cells)) ]
+          self.position = self.bandlimit-1
+      if self.mode=="wait": self.position = 0
+      if self.mode=="error": raise Exception("Reached out of bounds location")
+
+  def add_substract(self,value):
+    if self.position>=0:
+      self.cells[self.position] = self.cells[self.position]+value
+    else:
+      self.ncells[-self.position-1] = self.cells[-self.position-1]+value
+    if self.celllimit:
+      self.cells[self.position] = self.cells[self.position] % self.celllimit
+
+  def getValue(self):
+    if self.position>=0:
+      return self.cells[self.position]
+    else:
+      return self.ncells[-self.position-1]
+  def setValue(self,value):
+    value = value % self.celllimit if self.celllimit else value
+    if self.position>=0:
+      self.cells[self.position] = value
+    else:
+      self.ncells[-self.position-1] = value
+
+  def __str__(self):
+    r=str(self.position) + " :: "
+    if self.mode=="infinit":
+      r= r + str( self.ncells[::-1] ) + " :: "
+    return r + str(self.cells)
+    value = value % self.celllimit if self.celllimit else value
+    if self.position>=0:
+      self.cells[self.position] = value
+    else:
+      self.ncells[-self.position-1] = value
+
+def execute(code,data,outputMode,band):
   if outputMode not in ["raw", "int"]:
     raise Exception("Invalid outmode "+readmode)
 
-  code     =  cleanup( list(code) )
+  code     = cleanup( list(code) )
   bracemap = buildbracemap(code)
-  cells, codeptr, cellptr = [0], 0, 0
+  codeptr  = 0
 
   while codeptr < len(code):
     command = code[codeptr]
 
-    if command == ">":
-      cellptr += 1
-      if cellptr == len(cells): cells.append(0)
+    if command == ">":   band.movePositive()
+    if command == "<":   band.moveNegative()
+    if command == "+":   band.add_substract( 1)
+    if command == "-":   band.add_substract(-1)
+    if command == "[" and band.getValue() == 0: codeptr = bracemap[codeptr]
+    if command == "]" and band.getValue() != 0: codeptr = bracemap[codeptr]
 
-    if command == "<":
-      cellptr = 0 if cellptr <= 0 else cellptr - 1
-
-    if command == "+":
-      c = cells[cellptr]
-      cells[cellptr] = c + 1 if celllimit==0 else (c+1) % celllimit
-
-    if command == "-":
-      c = cells[cellptr]
-      cells[cellptr] = c - 1 if celllimit==0 else (c-1) % celllimit
-
-    if command == "[" and cells[cellptr] == 0: codeptr = bracemap[codeptr]
-    if command == "]" and cells[cellptr] != 0: codeptr = bracemap[codeptr]
     if command == ".":
       if outputMode=="raw":
-        sys.stdout.write(chr(cells[cellptr]))
+        sys.stdout.write(chr(band.getValue()))
       if outputMode=="int":
-        sys.stdout.write(str(cells[cellptr])+"\n")
+        sys.stdout.write(str(band.getValue())+"\n")
     if command == ",":
       c = data.getC()
       if c is None: break
-      cells[cellptr] = c if celllimit==0 else c % celllimit
+      band.setValue(c)
 
     codeptr += 1
 
@@ -162,13 +242,41 @@ def buildbracemap(code):
 
 def main():
   parser=argparse.ArgumentParser(epilog=helptext,formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument("--source",action="store",type=str,required=False,default=None,dest="codeFile",help="Input source file, should contain the source code")
-  parser.add_argument("--code",action="store",type=str,required=False,default=None,dest="inputCode",help="Input source file, should contain the source code")
-  parser.add_argument("--inmode",action="store",type=str,required=False,default="raw",dest="inputMode",help="How the input is read. Either raw or int for ASCII written integers")
-  parser.add_argument("--input",action="store",type=str,required=False,dest="input",help="input as argument. Read from it with the ',' command.")
-  parser.add_argument("--infile",action="store",type=str,required=False,dest="inFile",help="input as file. Read from it with the ',' command.")
-  parser.add_argument("--outmode",action="store",type=str,required=False,default="raw",dest="outputMode",help="How the values are outputed. Either raw or int for ASCII written integers")
-  parser.add_argument("--celllimit",action="store",type=int,required=False,default=0,dest="celllimit",help="Maximum value of a cell A limit implies that there are no negative values. 0 means no limit and cell can get negative")
+  h="Input source file, should contain the source code"
+  parser.add_argument("--source",   action="store",type=str,required=False,default=None,dest="codeFile",       help=h)
+  h="code, given as command line argument"
+  parser.add_argument("--code",     action="store",type=str,required=False,default=None,dest="inputCode",      help=h)
+  h="How the input is read. Either raw or int for ASCII written integers"
+  parser.add_argument("--inmode",   action="store",type=str,required=False,default="raw",dest="inputMode",     help=h)
+  h="input as argument. Read from it with the ',' command."
+  parser.add_argument("--input",    action="store",type=str,required=False,dest="input",                       help=h)
+  h="input as file. Read from it with the ',' command."
+  parser.add_argument("--infile",   action="store",type=str,required=False,dest="inFile",                      help=h)
+  h="How the values are outputed. Either raw or int for ASCII written integers"
+  parser.add_argument("--outmode",  action="store",type=str,required=False,default="raw",    dest="outputMode",help=h)
+  h="Maximum value of a cell A limit implies that there are no negative values. 0 means no limit and cell can get negative"
+  parser.add_argument("--celllimit",action="store",type=int,required=False,default=0,        dest="celllimit", help=h)
+  h=\
+  """
+    What happens when the data pointer moves 
+    this mode should either be infinit, wrap, wait or error
+    in infinit mode, new cells get added each time the pointer moves pass a border
+    in wrap mode, the position is calculated modulo bandlimit
+    in wait mode, the position stays at the border and doesn't move pass it
+    in error mode, passing beyond the border causes a error
+
+    the bandlimit set a maximum number of cells. Ignored in infinit mode
+    The bandlimit 0 means in this modes:
+    - inifinit: Ignore
+    - wrap: going below 0 means wrap to the highest current used. Grow when go beyond the positive border
+    - wait: going below 0 means wait at 0. Grow when go beyond the positive border
+    - error: going below 0 means error. Grow when go beyond the positive border
+  """
+  parser.add_argument("--bandmode", action="store",type=str,required=False,default="infinit",dest="bandmode", help=h)
+  h="How many cells are there at maximum. Interpretations Depends on bandmode"
+  parser.add_argument("--bandlimit",action="store",type=int,required=False,default=0,dest="bandlimit",         help=h)
+  h="Starting value of a cell"
+  parser.add_argument("--initvalue",action="store",type=int,required=False,default=0,dest="initValue",         help=h)
   args=parser.parse_args(sys.argv[1:])
 
   if args.inFile is None and args.input is None:
@@ -176,7 +284,8 @@ def main():
 
   code = Source( args.inputCode, args.codeFile )
   data = Source( args.input,     args.inFile,  args.inputMode )
-  execute( code, data, args.outputMode, args.celllimit )
+  band = Band( args.bandmode, args.bandlimit,  args.initValue, args.celllimit )
+  execute( code, data, args.outputMode, band )
 
 if __name__ == "__main__": main()
 
